341251a821f7c3244e7ea22ba9ac6a18f4256cd1
BlueHorn07 <bluehornblues@gmail.com>
2021-04-03 15:10:03 +0900

    refine some codes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,12 @@
 import { Controller, Get, Post } from '@nestjs/common';
-import { AuthService } from './auth/auth.service';
-import {AppService} from "./app.service";
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  sayHello(){
+  sayHello() {
     return this.appService.getHello();
   }
 }
