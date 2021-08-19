2e09faf71a3772371ea0d09f4603b2dcb335fa23
BlueHorn07 <bluehornblues@gmail.com>
2021-08-20 00:50:48 +0900

    update cors list

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       "http://localhost:3000", "http://localhost:3001",
-      "http://141.164.44.77", "http://141.164.44.77:1986",
-      "http://popo.postech.ac.kr", "http://popo.postech.ac.kr:1986",
-      "https://popo.postech.ac.kr", "https://popo.postech.ac.kr:1986"
+      "https://popo.poapper.com", "https://admin.popo.poapper.com",
     ],
     credentials: true
   });
