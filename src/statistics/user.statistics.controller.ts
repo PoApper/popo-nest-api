7a9aaa74912244be030982f59533490803bd4bfc
jjeongone <jjeongone@gmail.com>
2021-09-12 16:57:01 +0900

    add api tags

--- src/statistics/user.statistics.controller.ts
+++ src/statistics/user.statistics.controller.ts
@@ -1,14 +1,13 @@
-import {Controller, Get, Query} from "@nestjs/common";
-import {UserService} from "../popo/user/user.service";
-import {Between} from "typeorm";
+import { Controller, Get, Query } from '@nestjs/common';
+import { UserService } from '../popo/user/user.service';
+import { Between } from 'typeorm';
 import * as moment from 'moment';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Statistics')
 @Controller('statistics/user')
 export class UserStatisticsController {
-  constructor(
-    private readonly userService: UserService
-  ) {
-  }
+  constructor(private readonly userService: UserService) {}
 
   /**
    * format: GET statistics/user?start=YYYYMMDD&end=YYYYMMDD&format={YYYY | YYYYMM | YYYYMMDD}
@@ -21,14 +20,13 @@ export class UserStatisticsController {
     const data = {};
     while (query_idx.isBefore(query_end)) {
       data[query_idx.format('YYYY-MM')] = await this.userService.count({
-        createdAt: Between(query_idx.format(), query_idx.add(1, 'M').format())
-      })
+        createdAt: Between(query_idx.format(), query_idx.add(1, 'M').format()),
+      });
     }
 
     return {
-      "label": "user",
-      "data": data
-    }
+      label: 'user',
+      data: data,
+    };
   }
-
 }
