7c8415fbbac54cc951bfd58a2d076f996713f39f
bluehorn07 <bluehornblues@gmail.com>
2021-09-20 13:47:32 +0900

    update equip.reservation API

--- src/popo/reservation/equip/reserve.equip.controller.ts
+++ src/popo/reservation/equip/reserve.equip.controller.ts
@@ -9,6 +9,8 @@ import {
   Query,
   UseGuards,
 } from '@nestjs/common';
+import { ApiQuery, ApiTags } from '@nestjs/swagger';
+
 import { ReserveEquipService } from './reserve.equip.service';
 import { CreateReserveEquipDto } from './reserve.equip.dto';
 import { UserService } from '../../user/user.service';
@@ -18,7 +20,7 @@ import { UserType } from '../../user/user.meta';
 import { JwtAuthGuard } from '../../../auth/guards/jwt-auth.guard';
 import { Roles } from '../../../auth/authroization/roles.decorator';
 import { RolesGuard } from '../../../auth/authroization/roles.guard';
-import { ApiTags } from '@nestjs/swagger';
+import { EquipService } from '../../equip/equip.service';
 
 @ApiTags('Reservation Equip')
 @Controller('reservation-equip')
@@ -26,8 +28,16 @@ export class ReserveEquipController {
   constructor(
     private readonly reserveEquipService: ReserveEquipService,
     private readonly userService: UserService,
+    private readonly equipService: EquipService,
     private readonly mailService: MailService,
   ) {}
+
+  @Post('admin')
+  @UseGuards(JwtAuthGuard)
+  async create(@Body() dto: CreateReserveEquipDto) {
+    // admin 용 create
+    return this.reserveEquipService.save(dto);
+  }
 
   @Post()
   @UseGuards(JwtAuthGuard)
@@ -36,20 +46,54 @@ export class ReserveEquipController {
   }
 
   @Get()
-  get(@Query('owner') owner: string, @Query('status') status: string) {
+  @ApiQuery({ name: 'owner', required: false })
+  @ApiQuery({ name: 'status', required: false })
+  @ApiQuery({ name: 'date', required: false })
+  get(
+    @Query('owner') owner: string,
+    @Query('status') status: string,
+    @Query('date') date: string,
+  ) {
+    const whereOption = {};
     if (status) {
-      return this.reserveEquipService.find({
-        where: { reserveStatus: status },
-        order: { createdAt: 'DESC' },
-      });
-    } else if (owner) {
-      return this.reserveEquipService.find({
-        where: { owner: owner },
-        order: { createdAt: 'DESC' },
-      });
-    } else {
-      return this.reserveEquipService.find({ order: { createdAt: 'DESC' } });
+      whereOption['reserveStatus'] = status;
     }
+    if (owner) {
+      whereOption['owner'] = owner;
+    }
+    if (date) {
+      whereOption['date'] = date;
+    }
+
+    return this.reserveEquipService.find({
+      where: whereOption,
+      order: { createdAt: 'DESC' },
+    });
+  }
+
+  @Get('/owner/:ownerName')
+  @ApiQuery({ name: 'status', required: false })
+  @ApiQuery({ name: 'date', required: false })
+  async getByOwnerAndHideUserUuid(
+    @Param('ownerName') owner: string,
+    @Query('status') status: string,
+    @Query('date') date: string,
+  ) {
+    const whereOption = { owner: owner };
+    if (status) {
+      whereOption['reserveStatus'] = status;
+    }
+    if (date) {
+      whereOption['date'] = date;
+    }
+
+    const reservs = await this.reserveEquipService.find({
+      where: whereOption,
+      order: { createdAt: 'DESC' },
+    });
+
+    const refined1 = await this.hideUserUuid(reservs);
+    return this.hideEquipUuid(refined1);
   }
 
   @Get(':uuid')
@@ -91,5 +135,36 @@ export class ReserveEquipController {
         );
       }
     }
+  }
+
+  private async hideUserUuid(reservations) {
+    const refinedReservations = [];
+
+    for (const reservation of reservations) {
+      const user = await this.userService.findOne(reservation.user);
+      if (user) {
+        const { name } = user;
+        reservation.user = name;
+        refinedReservations.push(reservation);
+      }
+    }
+    return refinedReservations;
+  }
+
+  private async hideEquipUuid(reservations) {
+    const refinedReservations = [];
+    for (const reservation of reservations) {
+      const new_equips = [];
+      for (const equip_uuid of reservation.equips) {
+        const equip = await this.equipService.findOne(equip_uuid);
+        if (equip) {
+          const { name } = equip;
+          new_equips.push(name);
+        }
+      }
+      reservation.equips = new_equips;
+      refinedReservations.push(reservation);
+    }
+    return refinedReservations;
   }
 }
--- src/popo/reservation/equip/reserve.equip.entity.ts
+++ src/popo/reservation/equip/reserve.equip.entity.ts
@@ -1,39 +1,48 @@
-import {BaseEntity, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn} from "typeorm";
-import {ReservationStatus} from "../reservation.meta";
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { ReservationStatus } from '../reservation.meta';
 
 @Entity()
 export class ReserveEquip extends BaseEntity {
   @PrimaryGeneratedColumn('uuid')
   uuid: string;
 
-  @Column('simple-array', {nullable: false})
+  @Column('simple-array', { nullable: false })
   equips: string[]; // 장비의 uuid
 
-  @Column({nullable: false})
+  @Column({ nullable: false })
   user: string; // 예약한 유저의 uuid
 
-  @Column({nullable: false})
+  @Column({ nullable: false })
   owner: string; // 장비의 owner
 
-  @Column({nullable: false})
+  @Column({ nullable: false })
   phone: string; // 010-xxxx-xxxx
 
-  @Column({nullable: false})
+  @Column({ nullable: false })
   title: string;
 
-  @Column({nullable: false})
+  @Column({ nullable: false })
   description: string;
 
-  @Column({nullable: false})
-  date: number; // yyyy-MM-dd
+  @Column({ nullable: false })
+  // TODO: migration into YYYY.MM.DD
+  date: number; // yyyyMMdd
 
-  @Column({nullable: false})
-  startTime: number; // hh:mm
+  @Column({ nullable: false })
+  // TODO: migration into HH:MM
+  startTime: number; // hhmm
 
-  @Column({nullable: false})
-  endTime: number; // hh:mm
+  @Column({ nullable: false })
+  // TODO: migration into HH:MM
+  endTime: number; // hhmm
 
-  @Column({nullable: false, default: ReservationStatus.in_process})
+  @Column({ nullable: false, default: ReservationStatus.in_process })
   reserveStatus: ReservationStatus;
 
   @CreateDateColumn()
