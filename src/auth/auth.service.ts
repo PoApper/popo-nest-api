9b91a8971a0b48785e1800ee62691c8da0644a16
bluehorn07 <bluehornblues@gmail.com>
2021-09-27 22:29:24 +0900

    update jwt payload

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,8 @@
-import {Injectable, UnauthorizedException} from '@nestjs/common';
-import {JwtService} from '@nestjs/jwt';
-import * as crypto from "crypto";
-import {UserService} from "../popo/user/user.service";
-import {UserStatus} from "../popo/user/user.meta";
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as crypto from 'crypto';
+import { UserService } from '../popo/user/user.service';
+import { UserStatus } from '../popo/user/user.meta';
 
 /**
  * retrieving a user and verifying the password.
@@ -13,21 +13,20 @@ export class AuthService {
   constructor(
     private readonly usersService: UserService,
     private readonly jwtService: JwtService,
-  ) {
-  }
+  ) {}
 
   async validateUser(id: string, password: string): Promise<any> {
     const user = await this.usersService.findOneById(id);
     if (user) {
       const cryptoSalt = user.cryptoSalt;
 
-      if(user.userStatus != UserStatus.activated) {
-        throw new UnauthorizedException("Not activated account.")
+      if (user.userStatus != UserStatus.activated) {
+        throw new UnauthorizedException('Not activated account.');
       }
 
       const encryptedPassword = this.encryptPassword(password, cryptoSalt);
       if (user.password === encryptedPassword) {
-        const {password, ...result} = user;
+        const { password, ...result } = user;
         return result;
       }
     }
@@ -36,12 +35,19 @@ export class AuthService {
   }
 
   async generateJwtToken(user: any) {
-    const payload = {id: user.id, name: user.name, userType: user.userType};
+    const payload = {
+      uuid: user.uuid,
+      id: user.id,
+      name: user.name,
+      userType: user.userType,
+    };
     return this.jwtService.sign(payload);
   }
 
   // password encrypt util
   private encryptPassword(password: string, cryptoSalt: string) {
-    return crypto.pbkdf2Sync(password, cryptoSalt, 10000, 64, 'sha512').toString('base64');
+    return crypto
+      .pbkdf2Sync(password, cryptoSalt, 10000, 64, 'sha512')
+      .toString('base64');
   }
 }
--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,8 +1,8 @@
-import {Injectable} from '@nestjs/common';
-import {PassportStrategy} from '@nestjs/passport';
-import {ExtractJwt, Strategy} from 'passport-jwt';
-import {jwtConstants} from '../constants';
-import {Request} from "express";
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { jwtConstants } from '../constants';
+import { Request } from 'express';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -11,13 +11,19 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       jwtFromRequest: ExtractJwt.fromExtractors([
         (request: Request) => {
           return request?.cookies?.Authentication;
-        }]),
+        },
+      ]),
       ignoreExpiration: false,
       secretOrKey: jwtConstants.secret,
     });
   }
 
   async validate(payload: any) {
-    return {id: payload.id, name: payload.name, userType: payload.userType};
+    return {
+      uuid: payload.uuid,
+      id: payload.id,
+      name: payload.name,
+      userType: payload.userType,
+    };
   }
 }
