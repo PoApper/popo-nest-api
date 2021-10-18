be6a6cf822fa07419f628a296fac5a13129cec55
bluehorn07 <bluehornblues@gmail.com>
2021-10-19 01:19:52 +0900

    replace db config to AWS RDS

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,10 @@
 module.exports = {
-  type: 'mysql',
-  host: 'localhost',
-  port: 3306,
+  type: process.env.DB_TYPE,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
   entities: ['dist/**/*.entity.js'],
   synchronize: true,
 };
