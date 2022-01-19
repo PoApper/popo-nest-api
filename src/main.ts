a5fd5ed392a270858341ef8874af55a71e202409
bluehorn07 <bluehornblues@gmail.com>
2022-01-20 01:51:16 +0900

    change swagger page url

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,20 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import * as cookieParser from "cookie-parser";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
     origin: [
-      "http://localhost:3000", "http://localhost:3001",
-      "https://popo.poapper.com", "https://admin.popo.poapper.com",
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'https://popo.poapper.com',
+      'https://admin.popo.poapper.com',
+      'https://dev.popo.poapper.com',
     ],
-    credentials: true
+    credentials: true,
   });
 
   const swaggerConfig = new DocumentBuilder()
@@ -20,7 +23,7 @@ async function bootstrap() {
     .setVersion('0.1')
     .build();
   const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('swagger', app, document);
 
   await app.listen(4000);
 }
