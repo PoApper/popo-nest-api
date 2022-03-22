66727d25985397b2d676093cab7c92cd5b0ae965
jjeongone <jjeongone@gmail.com>
2022-03-23 01:21:10 +0900

    [fix] remove unnecessary change

--- src/popo/introduce/association/intro.association.dto.ts
+++ src/popo/introduce/association/intro.association.dto.ts
@@ -5,27 +5,3 @@ export class CreateIntroAssociationDto {
   readonly representative: string;
   readonly contact: string;
 }
-
-export class AssociationDto {
-  readonly contact: string;
-  readonly representative_contact: string;
-  readonly homepage_url: string;
-  readonly official_sns: string;
-  readonly file_url: string;
-}
-
-export class AssociationDescriptionDto {
-  readonly name: string;
-  readonly content: string;
-  readonly location: string;
-  readonly representative: string;
-  readonly language: string;
-  readonly association_uuid: string;
-}
-
-export class UpdateAssociationDescriptionDto {
-  readonly name: string;
-  readonly content: string;
-  readonly location: string;
-  readonly representative: string;
-}
