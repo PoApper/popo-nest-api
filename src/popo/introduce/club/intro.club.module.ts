e91394d12f8276a992d3e23f988f57c0120eb10a
bluehorn07 <bluehornblues@gmail.com>
2023-03-19 19:49:20 +0900

    fix module dependency missing

--- src/popo/introduce/club/intro.club.module.ts
+++ src/popo/introduce/club/intro.club.module.ts
@@ -1,14 +1,12 @@
-import { Module } from '@nestjs/common';
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {IntroClub} from "./intro.club.entity";
-import {IntroClubService} from "./intro.club.service";
-import {IntroClubController} from "./intro.club.controller";
+import { CacheModule, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { IntroClub } from './intro.club.entity';
+import { IntroClubService } from './intro.club.service';
+import { IntroClubController } from './intro.club.controller';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([IntroClub]),
-  ],
+  imports: [TypeOrmModule.forFeature([IntroClub]), CacheModule.register()],
   providers: [IntroClubService],
-  controllers: [IntroClubController]
+  controllers: [IntroClubController],
 })
 export class IntroClubModule {}
--- src/popo/whitebook/whitebook.module.ts
+++ src/popo/whitebook/whitebook.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Whitebook } from './whitebook.entity';
 import { WhitebookService } from './whitebook.service';
 import { WhitebookController } from './whitebook.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Whitebook])],
+  imports: [TypeOrmModule.forFeature([Whitebook]), CacheModule.register()],
   controllers: [WhitebookController],
   providers: [WhitebookService],
   exports: [WhitebookService],
