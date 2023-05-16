4159cbd768d45f6150627e9f11d21b59faaedc8d
bluehorn07 <bluehornblues@gmail.com>
2023-05-16 23:41:59 +0900

    generalize encryptWord

--- src/utils/encrypt-utils.ts
+++ src/utils/encrypt-utils.ts
@@ -1,7 +1,12 @@
 import crypto from 'crypto';
 
-export function encryptWord(word: string, cryptoSalt: string) {
+export function encryptWord(
+  word: string,
+  cryptoSalt: string,
+  iterations = 10000,
+  keyLen = 64,
+) {
   return crypto
-    .pbkdf2Sync(word, cryptoSalt, 10000, 64, 'sha512')
+    .pbkdf2Sync(word, cryptoSalt, iterations, keyLen, 'sha512')
     .toString('base64');
 }
