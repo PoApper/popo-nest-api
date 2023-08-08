4484b8d683ce26e58b259d1ab2876e2b1ff99273
bluehorn07 <bluehornblues@gmail.com>
2023-08-08 18:01:15 +0900

    fix mistake

--- src/utils/reservation-utils.ts
+++ src/utils/reservation-utils.ts
@@ -4,8 +4,8 @@ export function calculateReservationDurationMinutes(
   start_time: string,
   end_time: string,
 ): number {
-  const startMoment = moment(start_time, 'hhmm');
-  const endMoment = moment(end_time, 'hhmm');
+  const startMoment = moment(start_time, 'HHmm');
+  const endMoment = moment(end_time, 'HHmm');
   const momentDiff = moment.duration(endMoment.diff(startMoment));
 
   return momentDiff.asMinutes();
