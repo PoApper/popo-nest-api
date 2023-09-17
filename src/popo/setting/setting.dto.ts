b0d2a3afb453b97ffb8bf47ad542c9398f1a0552
HyoJeong Yun <hodori@postech.ac.kr>
2023-09-17 15:48:05 +0900

    New dongyeon setting variable (#72)

--- src/popo/setting/setting.dto.ts
+++ src/popo/setting/setting.dto.ts
@@ -1,4 +1,6 @@
 export class PopoSettingDto {
   readonly popo_crm_mail: string; // CRM = 고객 관계 관리
   readonly donyeon_bank: string; // ex. 카카오뱅크 12345-6789-10 (홍길동)
+  readonly dongyeon_service_time: string; // ex. 학기중 평일 12:30 ~ 13:30
+  readonly dongyeon_contact: string; // ex. (운영관리부) 0xx-xxxx-xxxx
 }
