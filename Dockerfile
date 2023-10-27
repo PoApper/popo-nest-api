f44f7a9f42313af7f32bb5a21b98490ee4f06350
Seokyun Ha <bluehornblues@gmail.com>
2023-10-27 23:07:53 +0900

    Show popo version (#75)

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,10 @@ COPY . .
 
 RUN npm run build
 
+# popo version
+ARG POPO_VERSION
+ENV POPO_VERSION ${POPO_VERSION}
+
 EXPOSE 4000
 
 CMD ["npm", "run", "start:prod"]
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `Hello POPO! (popo-${process.env.POPO_VERSION})`;
   }
 }
