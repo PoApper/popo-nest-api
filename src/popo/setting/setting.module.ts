f5a3a7e66164ff8391b5216b53986c77c6ce8600
bluehorn07 <bluehornblues@gmail.com>
2023-11-15 23:30:12 +0900

    add missed module import

--- src/popo/setting/setting.module.ts
+++ src/popo/setting/setting.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { NestjsFormDataModule } from 'nestjs-form-data';
+
 import { SettingController } from './setting.controller';
 import { FileModule } from '../../file/file.module';
 
 @Module({
-  imports: [FileModule],
+  imports: [FileModule, NestjsFormDataModule],
   controllers: [SettingController],
 })
 export class SettingModule {}
