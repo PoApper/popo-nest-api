82f0bb02d37168b314521a5157e45e1a8ac20c63
bluehorn07 <bluehornblues@gmail.com>
2024-02-17 17:08:44 +0900

    update rc-student-list get status API

--- src/popo/setting/setting.service.ts
+++ src/popo/setting/setting.service.ts
@@ -17,24 +17,39 @@ export class SettingService {
   resetRcStudentsUserType() {
     return this.userRepo.update({userType: UserType.rc_student}, {userType: UserType.student});
   }
-  
+
   async countRcStduentsList() {
     const ret = await this.fileService.queryOnS3('popo-rc-students-list.csv', 'SELECT COUNT(*) AS cnt FROM S3Object s');
     return ret[0]['cnt'];
   }
-  
-  getRcStduentsList() {
-    return this.fileService.queryOnS3('popo-rc-students-list.csv', 'SELECT email FROM S3Object s');
+
+  async getRcStduentsStatus() {
+    const rc_list = await this.fileService.queryOnS3('popo-rc-students-list.csv', "SELECT name, TRIM(TRAILING '\r' FROM email) AS email FROM S3Object s");
+
+    for (const row of rc_list) {
+      const email = row['email'];
+      const user = await this.userRepo.findOneBy({email: email});
+      if (user) {
+        row['status'] = 'registered';
+        row['user_name'] = user['name'];
+        row['user_type'] = user['userType'];
+        row['created_at'] = user['createdAt'];
+      } else {
+        row['status'] = 'not_registered';
+      }
+    }
+
+    return rc_list;
   }
-  
+
   async checkRcStudent(email: string) {
-    const ret = await this.fileService.queryOnS3('popo-rc-students-list.csv', `SELECT email FROM S3Object s WHERE s.email = '${email}'`);
+    const ret = await this.fileService.queryOnS3('popo-rc-students-list.csv', `SELECT TRIM(TRAILING '\r' FROM email) AS email FROM S3Object s WHERE s.email = '${email}'`);
     return ret.length > 0;
   }
 
   async setRcStudentsUserTypeByCsv() {
     // Get email list from csv
-    const queryRet = await this.getRcStduentsList();
+    const queryRet = await this.fileService.queryOnS3('popo-rc-students-list.csv', "SELECT TRIM(TRAILING '\r' FROM email) AS email FROM S3Object s");
 
     let updatedUserCnt = 0;
     for(const row of queryRet) {
