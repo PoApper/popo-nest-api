8f672d62177e731666166efcf61cff8ca7dd0b62
Gwanho Kim <khkim6040@postech.ac.kr>
2024-05-12 16:48:54 +0900

    Add auth.controller tests

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -6,6 +6,8 @@ import { ReservePlaceService } from '../popo/reservation/place/reserve.place.ser
 import { ReserveEquipService } from '../popo/reservation/equip/reserve.equip.service';
 import { MailService } from 'src/mail/mail.service';
 import { createMock, DeepMocked } from '@golevelup/ts-jest';
+import { BadRequestException } from '@nestjs/common';
+import { UserStatus } from 'src/popo/user/user.meta';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -57,5 +59,133 @@ describe('AuthController', () => {
     expect(reservePlaceService).toBeDefined();
     expect(reserveEquipService).toBeDefined();
     expect(mailService).toBeDefined();
+  });
+
+  it('should reject unauthorized admin user', async () => {
+    const req = {
+      path: 'admin',
+      user: {
+        userType: ['user'],
+      },
+    };
+    await expect(controller.verifyToken(req as any)).rejects.toThrow();
+  });
+
+  it('should verify token and return user', async () => {
+    const req = {
+      path: 'me',
+      user: {
+        uuid: '1',
+      },
+    };
+    await expect(controller.verifyToken(req as any)).resolves.toEqual(req.user);
+  });
+
+  it('should get own reservations', async () => {
+    const req = {
+      user: {
+        uuid: '1',
+        email: 'email',
+      },
+    };
+    const user = req.user;
+    userService.findOneByEmail.mockResolvedValue(user as any);
+    const placeReserve = [{ uuid: '1' }];
+    const equipReserve = [{ uuid: '1' }];
+    reservePlaceService.find.mockResolvedValue(placeReserve as any);
+    reserveEquipService.find.mockResolvedValue(equipReserve as any);
+    await expect(controller.getOwnReservations(req as any)).resolves.toEqual({
+      place_reservation: placeReserve,
+      equip_reservation: equipReserve,
+    });
+  });
+
+  // TODO: Test login
+
+  // TODO: Test logout
+
+  it('should register user if verification mail has been sent successfully', async () => {
+    const req = {
+      body: {
+        email: 'email',
+        password: 'password',
+        name: 'name',
+      },
+    };
+    const user = req.body;
+    userService.save.mockResolvedValue(user as any);
+    mailService.sendVerificationMail.mockResolvedValue(undefined);
+    await expect(controller.register(req as any)).resolves.toEqual(user);
+  });
+
+  it('should throw error if verification mail has not been sent successfully', async () => {
+    const req = {
+      body: {
+        email: 'email',
+        password: 'password',
+        name: 'name',
+      },
+    };
+    const user = req.body;
+    userService.save.mockResolvedValue(user as any);
+    mailService.sendVerificationMail.mockRejectedValue(
+      new BadRequestException(),
+    );
+    await expect(controller.register(req as any)).rejects.toThrow();
+  });
+
+  it('should throw error if user does not exist', async () => {
+    const req = {
+      body: {
+        email: 'email',
+      },
+    };
+    userService.findOneByEmail.mockResolvedValue(undefined);
+    await expect(controller.resetPassword(req as any)).rejects.toThrow();
+  });
+
+  it('should throw error if user status is password_reset', async () => {
+    const req = {
+      body: {
+        email: 'email',
+      },
+    };
+    const user = {
+      userStatus: UserStatus.password_reset,
+    };
+    userService.findOneByEmail.mockResolvedValue(user as any);
+    await expect(controller.resetPassword(req as any)).rejects.toThrow();
+  });
+
+  it('should reset password', async () => {
+    const req = {
+      body: {
+        email: 'email',
+      },
+    };
+    const user = {
+      uuid: '1',
+    };
+    userService.findOneByEmail.mockResolvedValue(user as any);
+    userService.updatePasswordByEmail.mockResolvedValue(undefined);
+    userService.updateUserStatus.mockResolvedValue(undefined);
+    mailService.sendPasswordResetMail.mockResolvedValue(undefined);
+    await expect(controller.resetPassword(req as any)).resolves.not.toThrow();
+  });
+
+  it('should return user informatio', async () => {
+    const req = {
+      user: {
+        uuid: '1',
+      },
+    };
+    const userInfo = {
+      email: 'email',
+      name: 'name',
+      userType: 'user',
+      userStatus: 'activated',
+    };
+    userService.findOneByUuid.mockResolvedValue(userInfo as any);
+    await expect(controller.getMyInfo(req as any)).resolves.toEqual(userInfo);
   });
 });
