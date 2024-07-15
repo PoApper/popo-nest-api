86e62e28fd7fc9070bd4e3013c97359722a0e050
Gwanho Kim <khkim6040@postech.ac.kr>
2024-07-15 19:12:31 +0900

    Delete unused library

--- src/popo/like/like.module.ts
+++ src/popo/like/like.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { NestjsFormDataModule } from 'nestjs-form-data';
 
-import { FileModule } from '../../file/file.module';
 import { Like } from './like.entity';
 import { LikeController } from './like.controller';
 import { LikeService } from './like.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Like]), NestjsFormDataModule, FileModule],
+  imports: [TypeOrmModule.forFeature([Like])],
   controllers: [LikeController],
   providers: [LikeService],
   exports: [LikeService],
