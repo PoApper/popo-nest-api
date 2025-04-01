92083b7aaa2412cee4a802d094ec7c7c28d52897
Seokyun Ha <bluehornblues@gmail.com>
2025-04-01 10:37:47 +0900

    feat: User reservation pagination (#130)

--- src/popo/reservation/place/reserve.place.controller.ts
+++ src/popo/reservation/place/reserve.place.controller.ts
@@ -137,14 +137,32 @@ export class ReservePlaceController {
 
   @Get('user')
   @UseGuards(JwtAuthGuard)
-  async getMyReservation(@Req() req: Request) {
+  @ApiQuery({ name: 'skip', required: false })
+  @ApiQuery({ name: 'take', required: false })
+  async getMyReservation(
+    @Req() req: Request,
+    @Query('skip') skip: number,
+    @Query('take') take: number,
+  ) {
     const user = req.user as JwtPayload;
 
-    const reservations = await this.reservePlaceService.find({
+    const findOption = {
       where: { booker_id: user.uuid },
       order: { date: 'DESC', start_time: 'DESC' },
+    };
+
+    const total = await this.reservePlaceService.count({
+      booker_id: user.uuid,
     });
-    return this.reservePlaceService.joinPlace(reservations);
+
+    findOption['skip'] = skip ?? 0;
+    findOption['take'] = take ?? 10;
+
+    const reservations = await this.reservePlaceService.find(findOption);
+    return {
+      items: await this.reservePlaceService.joinPlace(reservations),
+      total: total,
+    };
   }
 
   @Get('user/:uuid')
