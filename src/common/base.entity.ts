89f566839f44a0fbd2b7c7c55987a6aa8c8e4dc9
Gwanho Kim <khkim6040@gmail.com>
2025-08-15 17:08:10 +0900

    Feat: Base entity

---
+++ src/common/base.entity.ts
@@ -0,0 +1,18 @@
+import { CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { ApiHideProperty } from '@nestjs/swagger';
+import { Exclude } from 'class-transformer';
+
+// 날짜 관련 칼럼을 가지는 모든 엔티티의 기본 클래스
+// @ApiHideProperty() 어노테이션을 붙이면 swagger에서 해당 칼럼을 보이지 않게 할 수 있음
+// @Exclude() 어노테이션을 붙이면 컨트롤러에서 리턴 시 해당 칼럼을 보이지 않게 할 수 있음
+export abstract class Base {
+  @CreateDateColumn()
+  @ApiHideProperty()
+  @Exclude()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  @ApiHideProperty()
+  @Exclude()
+  updatedAt: Date;
+}
