ede0f663f94c9d342385bb26b63c7b6df685f28e
Gwanho Kim <khkim6040@gmail.com>
2025-09-07 18:20:14 +0900

    Fix: Set alias user table

--- src/popo/user/user.service.ts
+++ src/popo/user/user.service.ts
@@ -80,14 +80,23 @@ export class UserService {
   }
 
   searchCountByKeyword(keyword = '') {
-    const qb = this.userRepo.createQueryBuilder();
+    const qb = this.userRepo.createQueryBuilder('user');
 
     return qb
       .select('COUNT(*) AS count')
-      .where(`LOWER(name) LIKE '%${keyword}%'`)
-      .orWhere(`LOWER(email) LIKE '%${keyword}%'`)
-      .orWhere(`LOWER(userType) LIKE '%${keyword}%'`)
-      .orderBy('lastLoginAt', 'DESC')
+      .where(`LOWER(user.name) LIKE :keyword`, {
+        keyword: `%${keyword}%`,
+      })
+      .orWhere(`LOWER(user.email) LIKE :keyword`, {
+        keyword: `%${keyword}%`,
+      })
+      .orWhere(`LOWER(user.userType) LIKE :keyword`, {
+        keyword: `%${keyword}%`,
+      })
+      .orWhere(`LOWER(user.userType) LIKE :keyword`, {
+        keyword: `%${keyword}%`,
+      })
+      .orderBy('user.lastLoginAt', 'DESC')
       .getRawOne();
   }
 
